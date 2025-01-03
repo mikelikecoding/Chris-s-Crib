cf30771be745c7984e5308ab079422f138aaa0d7
Michael Calvin Holmes <michaelcalvinholmes@Michaels-MacBook-Air.local>
2025-01-02 23:19:58 -0800

    Update Vite config for deployment

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -2,6 +2,7 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
+  base: '/Chris-Crib/', // Add the base path for GitHub Pages
   plugins: [react()],
   root: '.', // Ensures Vite starts from the root
   build: {
